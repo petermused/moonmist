aa55aec2fc65c56aab44524704ec4960c8852e55
Peter Medus <16763503+petermused@users.noreply.github.com>
2026-05-03 15:14:06 +0100

    Add /nix directory

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -19,6 +16,5 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Add mutable /nix directory to root so I can install Lix
+install -d -m 0755 /nix
